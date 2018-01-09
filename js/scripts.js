fa6b75db4c20531926955281d9d4ed70cd2b1dde
Jamie Pittak and Manuela Garcia <manuela@>
2018-01-09 09:23:27 -0800

    create clickable links to hide and show image.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
-  $("p").click(function(){
-    $(".walrus-showing").show();
-    $(".walrus-hidden").hide();
+  $(".clickable").click(function(){
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
 
   });
-
-
 });
